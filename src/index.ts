c97e074b5177c082c0e8ac565cf9bf19ee548efc
mtti <mtti@users.noreply.github.com>
2020-01-21 22:18:56 +0200

    Export migration type

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+export * from './Migration';
 export * from './Migrator';
 export * from './PostgresMigrator';
